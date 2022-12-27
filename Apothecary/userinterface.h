2e4ba95f7e4fbb78b7fc12af4c6866b070d0cd94
Anna <koyvestoynen@bk.ru>
2022-12-27 23:00:18 +0300

    Add changes user interface

--- Apothecary/userinterface.h
+++ Apothecary/userinterface.h
@@ -2,15 +2,33 @@
 #define USERINTERFACE
 
 #include "cstdio"
+#include <apothecary.h>
+#include <salesinvoice.h>
+
 #include <iostream>
 
+// интерфейс
 class UserInterface
 {
 private:
+
+    // указатели на все документы
+
+    Stock* ptrStok;
+    StockInputScreen* ptrStokInputScreen;
+
+    SalesInvoiceRecord* ptrSalesInvoiceRecord;
+    SalesInvoiceInputSceen* ptrSalesInputScreen;Ђаа
+
     char ch;
 
 public:
 
+    // конструктор и деструктор
+    UserInterface();
+    ~UserInterface();
+
+    // отображение интерфейса
     void interact();
 
 };
