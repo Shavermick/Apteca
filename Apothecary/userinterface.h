f99db15a54b53cb54610005952fe4a77573aa047
Anna <koyvestoynen@bk.ru>
2022-12-27 21:17:07 +0300

    Add userinterface.h

---
+++ Apothecary/userinterface.h
@@ -0,0 +1,19 @@
+#ifndef USERINTERFACE
+#define USERINTERFACE
+
+#include "cstdio"
+#include <iostream>
+
+class UserInterface
+{
+private:
+    char ch;
+
+public:
+
+    void interact();
+
+};
+
+#endif // USERINTERFACE
+
